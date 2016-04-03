16baab090572d8e4a1a21a943eb3b3b8587531ba
Thomas Lindstrøm <t@hom.as>
2016-04-03 05:07:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# empty-string
+
+a well-tested empty string module in case you forget how to make one
+
+```
+npm install empty-string
+```
+
+```javascript
+var EMPTY_STRING = require('empty-string');
+console.log(EMPTY_STRING); // ''
+```
---
+++ test.js
@@ -0,0 +1,52 @@
+// ---------------------------------------------------------------------------
+//  assert.js
+// ---------------------------------------------------------------------------
+
+    var EMPTY_STRING = require('./index.js');
+    var assert = require('assert');
+
+    describe('empty-string', function() {
+        it('should be an empty string', function () {
+            assert.equal(EMPTY_STRING, '');
+        });
+
+        it('should not be an empty object', function () {
+            assert.equal(EMPTY_STRING === {}, false);
+        });
+
+        it('should not be an empty array', function () {
+            assert.equal(EMPTY_STRING === [], false);
+        });
+
+        it('should not be a string with content', function () {
+            assert.equal(EMPTY_STRING === 'content', false);
+        });
+
+        it('should not be the number 1', function () {
+            assert.equal(EMPTY_STRING === 1, false);
+        });
+
+        it('should not be the number 10000 either', function () {
+            assert.equal(EMPTY_STRING === 10000, false);
+        });
+
+        it('should not be the number 9001 either', function () {
+            assert.equal(EMPTY_STRING === 9001, false);
+        });
+
+        it('should not be undefined', function () {
+            assert.equal(EMPTY_STRING === undefined, false);
+        });
+
+        it('should not be false', function () {
+            assert.equal(EMPTY_STRING === false, false);
+        });
+
+        it('should not be true', function () {
+            assert.equal(EMPTY_STRING === true, false);
+        });
+
+        it('should be itself', function () {
+            assert.equal(EMPTY_STRING, EMPTY_STRING);
+        });
+    });
